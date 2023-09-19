61344449231f2eb0ed157b67118881ca2ba7c2db
tamilpp25 <tamilpp25@gmail.com>
2023-09-20 02:28:07 +0530

    add fetch method

--- src/env.mjs
+++ src/env.mjs
@@ -17,6 +17,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    YT_API_KEY: z
+      .string()
   },
 
   /**
@@ -35,6 +37,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    YT_API_KEY: process.env.YT_API_KEY
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
---
+++ src/pages/api/fetch.ts
@@ -0,0 +1,43 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { YoutubeDataAPI } from "youtube-v3-api";
+import { env } from "~/env.mjs";
+
+const api = new YoutubeDataAPI(env.YT_API_KEY);
+
+interface Result {
+  id: {
+    kind: string;
+    videoId: string;
+  };
+  snippet: {
+    title: string;
+    description: string;
+    channelTitle: string;
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+  try {
+    const results = (await api.searchAll(req.query.search as string, Number.parseInt(req.query.limit as string), {
+      type: 'video',
+    })).items as Result[]
+
+    const output = results.map(e => {
+      return {
+        name: e.snippet.channelTitle,
+        title: e.snippet.title
+      }
+    }).filter(e => {
+      return e.name.includes(req.query.filter as string)
+    })
+
+    res.status(200).json({ output });
+  } catch (err) {
+    res.status(500).json({
+      code: -1,
+      msg: 'Internal Server Error'
+    })
+  }
+
+}
