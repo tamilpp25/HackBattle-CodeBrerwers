a4277390dbfc43c9d7744abdb59fb92737c34853
Naveen <naveenramesh2003@gmail.com>
2023-09-20 18:37:51 +0530

    pyqs creation

---
+++ src/pages/pyqs.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function pyqs() {
+    return (
+    <>
+    <Head>
+        <title>TeachSync</title>
+        <meta name="description" content="TeachSync" />
+        <link rel="icon" href="/favicon.ico" /> 
+    </Head>
+    <main className="min-h-screen flex items-center justify-center overflow-hidden relative">
+    
+    </main>
+    </>
+    )
+}
